17b57193cff35b1a563fb0ce250826bb575f8fe2
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-06-10 16:31:52 -0400

    Fix formatting of SEC data on load

--- memory/loadData.ts
+++ memory/loadData.ts
@@ -14,12 +14,24 @@ export const loadSECdata = async (): Promise<void> => {
 
 	const raw = resp.data;
 
-	const tickers: Ticker[] = Object.values(raw).map((e: any) => ({
-		cik: e.cik,
-		name: e.title,
-		ticker: e.ticker,
-		exchange: e.exchange,
+	if (!raw.fields || !raw.data) throw new Error("Invalid SEC data format");
+
+	const [CIK_IDX, NAME_IDX, TICKER_IDX, EXCHANGE_IDX] = [
+		raw.fields.indexOf("cik"),
+		raw.fields.indexOf("name"),
+		raw.fields.indexOf("ticker"),
+		raw.fields.indexOf("exchange"),
+	];
+
+	const tickers: Ticker[] = raw.data.map((row: any[]) => ({
+		cik: row[CIK_IDX],
+		name: row[NAME_IDX],
+		ticker: row[TICKER_IDX],
+		exchange: row[EXCHANGE_IDX],
 	}));
 
 	store.data = tickers;
+	console.log(
+		`✅ Loaded ${store.data.length} tickers to store from ${tickers.length} SEC data rows`
+	);
 };
