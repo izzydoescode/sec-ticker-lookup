50d673d2918c2edd038a3b7ca042b8aa621e29a6
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-06-10 15:01:50 -0400

    Add tickers + activity routers, load api data, add healthcheck

--- app.ts
+++ app.ts
@@ -1,11 +1,20 @@
-import express from "express";
-import usersRouter from "./routes/users";
+import express, { Request, Response } from "express";
+import tickersRouter from "./routes/tickers";
+import activityRouter from "./routes/activity";
+import { loadSECdata } from "./memory/loadData";
 
 const app = express();
 app.use(express.json());
-app.use("/users", usersRouter);
+app.use("/tickers", tickersRouter);
+app.use("/activity", activityRouter);
+(async () => {
+	await loadSECdata();
+	const PORT = process.env.PORT || 3000;
+	app.listen(PORT, () => {
+		console.log(`Server running on port ${PORT}`);
+	});
+})();
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+app.post("/", (req: Request, res: Response) => {
+	return res.send("OK");
 });
