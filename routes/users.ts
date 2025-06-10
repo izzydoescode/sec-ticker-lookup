1ddb63ccb1a7a647112c7f6705fd68ea3cf65a8e
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-06-10 16:33:00 -0400

    Delete users routes and tests

--- routes/users.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from "express";
-import { store } from "../memory/store";
-import { User, UserCreateRequest } from "../types/User";
-
-const router = express.Router();
-
-// Logic will be added live during the interview
-
-export default router;
