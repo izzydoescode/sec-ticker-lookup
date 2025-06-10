3016aa969359157c01f1a1e3d64e472df7412f62
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-06-10 11:33:52 -0400

    Update jest.config file

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,11 @@ const { createDefaultPreset } = require("ts-jest");
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
-/** @type {import("jest").Config} **/
+/** @type {import("ts-jest").JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    ...tsJestTransformCfg,
-  },
+	preset: "ts-jest",
+	testEnvironment: "node",
+	transform: {
+		...tsJestTransformCfg,
+	},
 };
